6812eb8dbec61a36e00ce154a8295ce6c323b525
artem.lezgyan <artem.lezgyan@exactprosystems.com>
2021-05-12 20:05:01 +0400

    init data from report data provider

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#   Copyright 2020-2020 Exactpro (Exactpro Systems Limited)
+#   Copyright 2020-2021 Exactpro (Exactpro Systems Limited)
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -111,13 +111,13 @@ setup(
     description=package_name,
     long_description=long_description,
     long_description_content_type='text/markdown',
-    author='TH2-devs',
-    author_email='th2-devs@exactprosystems.com',
-    url='https://github.com/th2-net/th2-grpc-generator-template',
+    author='Artem Lezgyan',
+    author_email='artem.lezgyan@exactprosystems.com',
+    url='https://github.com/th2-net/th2-rpt-data-provider',
     license='Apache License 2.0',
     python_requires='>=3.7',
     install_requires=[
-        'grpcio-tools==1.33.1'
+        'th2-grpc-common~=3.1.2'
     ],
     packages=packages,
     package_data=package_data,
